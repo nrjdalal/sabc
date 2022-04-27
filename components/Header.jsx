a37148954a9872485b56767477577e8c8e6eb2f1
Neeraj Dalal <admin@nrjdalal.com>
2022-04-27 15:50:34 +0530

    Roadmap added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,56 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+
 const Header = () => {
   return (
-    <nav className="absolute top-0 flex h-16 w-full items-center justify-between bg-gray-800 p-6">
-      <img
-        src="https://storage.googleapis.com/production-bluehost-v1-0-2/782/1192782/xZ9n2NCd/17c82326a49949968e98b67aa7550260"
-        alt=""
-        className="h-12"
-      />
+    <nav className="fixed top-0 z-10 flex h-16 w-full items-center justify-between bg-gray-800 p-6">
+      <Link href="/">
+        <a>
+          <img
+            src="https://storage.googleapis.com/production-bluehost-v1-0-2/782/1192782/xZ9n2NCd/17c82326a49949968e98b67aa7550260"
+            alt=""
+            className="h-12"
+          />
+        </a>
+      </Link>
+
       <div className="flex items-center space-x-6">
         <ul className="flex space-x-6 text-base">
-          <li>Home</li>
-          <li>About</li>
-          <li>$OSARU</li>
-          <li>Zippo</li>
-          <li>Stake</li>
-          <li>Breed</li>
-          <li>Roadmap</li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/osaru">
+              <a>$OSARU</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/zippo">
+              <a>Zippo</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/stake">
+              <a>Stake</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/breed">
+              <a>Breed</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/roadmap">
+              <a>Roadmap</a>
+            </Link>
+          </li>
         </ul>
         <button className="rounded-sm bg-yellow-300 p-3 py-1.5 text-base text-black">Discord</button>
       </div>
