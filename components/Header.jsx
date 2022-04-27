a72b680ea4ba89a9e00ff6b3affbf183a3c826ea
Neeraj Dalal <admin@nrjdalal.com>
2022-04-27 19:04:42 +0530

    header tuning added

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <nav className="fixed top-0 z-10 flex h-16 w-full items-center justify-between bg-black p-6">
-      <Link href="/">
+    <nav className="fixed top-0 z-10 flex h-16 w-full items-center justify-center bg-black p-6">
+      {/* <Link href="/">
         <a>
           <img
             src="https://storage.googleapis.com/production-bluehost-v1-0-2/782/1192782/xZ9n2NCd/17c82326a49949968e98b67aa7550260"
@@ -12,15 +12,15 @@ const Header = () => {
             className="h-12"
           />
         </a>
-      </Link>
+      </Link> */}
 
       <div className="flex items-center space-x-6">
         <ul className="flex space-x-6 text-base">
-          <li>
+          {/* <li>
             <Link href="/">
               <a>Home</a>
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link href="/about">
               <a>About</a>
@@ -52,7 +52,7 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button className="rounded-sm bg-yellow-300 p-3 py-1.5 text-base text-black">Discord</button>
+        {/* <button className="rounded-sm bg-yellow-300 p-3 py-1.5 text-base text-black">Discord</button> */}
       </div>
     </nav>
   )
