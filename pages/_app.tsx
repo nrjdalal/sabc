0067794bb636dfe5fcd678bed22823989b268f24
Neeraj Dalal <admin@nrjdalal.com>
2022-04-27 18:30:24 +0530

    Background added added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Header from 'components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="relative overflow-x-hidden bg-gray-800 font-nunito text-lg font-light tracking-wide text-white">
+    <div className="relative overflow-x-hidden bg-gradient-to-br from-blue-900 via-slate-900  to-purple-900 font-nunito text-lg font-light tracking-wide text-white">
       <Header />
       <Component {...pageProps} />
     </div>
