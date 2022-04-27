d3821a039a5305de4694ab8f52f28df4abfd146d
Neeraj Dalal <admin@nrjdalal.com>
2022-04-27 13:53:47 +0530

    Add fonts anton and nunito

---
+++ pages/roadmap.jsx
@@ -0,0 +1,9 @@
+const roadmap = () => {
+  return (
+    <div className="bg-black p-4 md:p-16">
+      <div className="font-anton text-4xl text-yellow-300">OUR ROADMAP</div>
+    </div>
+  )
+}
+
+export default roadmap
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: ["'Anton'", 'sans-serif'],
+        nunito: ["'Nunito'", 'sans-serif'],
+      },
+    },
   },
-  plugins: [
-    '@tailwindcss/forms',
-    '@tailwindcss/line-clamp',
-    '@tailwindcss/typography',
-  ],
+  plugins: ['@tailwindcss/forms', '@tailwindcss/line-clamp', '@tailwindcss/typography'],
 }
